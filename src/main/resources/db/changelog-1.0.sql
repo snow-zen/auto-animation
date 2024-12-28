8a0a5071f33cfb7afcdd4636dcd5c9b6b45d5587
snow-zen <2372912451zxj@gmail.com>
2024-12-28 23:33:39 +0800

    build: 添加 liquibase 组件

--- src/main/resources/db/changelog-1.0.sql
+++ src/main/resources/db/changelog-1.0.sql
@@ -1,3 +1,6 @@
+-- liquibase formatted sql
+
+-- changeset snow-zen:1
 create table task
 (
     id     long         not null auto_increment comment '主键id',
@@ -5,3 +8,4 @@ create table task
     status varchar(100) not null comment '任务状态',
     primary key (id)
 );
+-- rollback drop table if exists task;
