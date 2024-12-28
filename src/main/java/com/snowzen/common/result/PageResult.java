0ae34b932ea0f370813967b69aee4b90f58d2685
snow-zen <2372912451zxj@gmail.com>
2024-12-28 22:44:25 +0800

    refactor: dealing with checkstyle scanning issues

--- src/main/java/com/snowzen/common/result/PageResult.java
+++ src/main/java/com/snowzen/common/result/PageResult.java
@@ -3,6 +3,12 @@ package com.snowzen.common.result;
 import java.util.List;
 
 /**
+ * 分页结果。
+ *
+ * @param page  当前分页页码。
+ * @param data  当前分页数据列表。
+ * @param count 所有数据统计量。
+ * @param <T>   分页数据具体类型。
  * @author snow-zen
  */
 public record PageResult<T>(Integer page, List<T> data, Long count) {
--- src/main/java/com/snowzen/repository/TaskRepository.java
+++ src/main/java/com/snowzen/repository/TaskRepository.java
@@ -9,14 +9,22 @@ import io.quarkus.panache.common.Page;
 import jakarta.enterprise.context.ApplicationScoped;
 
 /**
+ * 任务存储库。
+ *
  * @author snow-zen
  */
 @ApplicationScoped
 public class TaskRepository implements PanacheRepository<TaskEntity> {
 
+    /**
+     * 分页查询任务信息。
+     *
+     * @param pageSpec 分页说明。
+     * @return 分页结果。
+     */
     public PageResult<TaskEntity> page(PageSpec pageSpec) {
         PanacheQuery<TaskEntity> query = findAll()
-            .page(Page.of(pageSpec.getPage(), pageSpec.getSize()));
-        return new PageResult<>(pageSpec.getPage(), query.list(), query.count());
+            .page(Page.of(pageSpec.page(), pageSpec.size()));
+        return new PageResult<>(pageSpec.page(), query.list(), query.count());
     }
 }
