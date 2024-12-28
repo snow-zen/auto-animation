380c6b59420c8e64acc0cba7b6fd5b54ad9c879b
snow-zen <2372912451zxj@gmail.com>
2024-12-28 19:13:26 +0800

    初始化项目

---
+++ src/test/java/com/snowzen/ExampleResourceTest.java
@@ -0,0 +1,20 @@
+package com.snowzen;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+class ExampleResourceTest {
+    @Test
+    void testHelloEndpoint() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello from Quarkus REST"));
+    }
+
+}
