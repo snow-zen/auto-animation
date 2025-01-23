6dca9de39874c398c8fecc384a599b7ee309537a
snow-zen <2372912451zxj@gmail.com>
2025-01-23 14:58:19 +0800

    feat: 未知异常打印到错误日志

--- src/main/java/com/snowzen/configuration/error/GlobalExceptionMapper.java
+++ src/main/java/com/snowzen/configuration/error/GlobalExceptionMapper.java
@@ -5,16 +5,19 @@ import com.snowzen.common.ApiResponseStatus;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * {@link Throwable} 异常对应的响应处理器。
  *
  * @author snow-zen
  */
+@Slf4j
 @Provider
 public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
     @Override
     public Response toResponse(Throwable exception) {
+        log.error("未知异常", exception);
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
             .entity(
                 ApiResponse.buildWithoutDetail(
