7d97aa97383230b99f23169aaceda663f970da0b
snow-zen <2372912451zxj@gmail.com>
2025-01-23 14:58:41 +0800

    feat: 添加 jakarta.ws.rs.NotFoundException 异常处理器

---
+++ src/main/java/com/snowzen/configuration/error/NotFoundExceptionMapper.java
@@ -0,0 +1,26 @@
+package com.snowzen.configuration.error;
+
+import com.snowzen.common.ApiResponse;
+import com.snowzen.common.ApiResponseStatus;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+/**
+ * @author snow-zen
+ */
+@Provider
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+    @Override
+    public Response toResponse(NotFoundException exception) {
+        return Response.status(Response.Status.NOT_FOUND)
+            .entity(
+                ApiResponse.buildWithoutDetail(
+                    ApiResponseStatus.DATA_NOT_FOUND,
+                    "未找到资源"
+                )
+            )
+            .build();
+    }
+}
